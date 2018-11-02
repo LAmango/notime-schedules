b75b47ad754e76f53ce8930093dc3063b52b1687
Lucas Albano <lucas@cenersys.com>
2018-11-02 14:14:27 -0400

    added forms functionality with vailitity checker

--- flasktest.py
+++ flasktest.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import WeekDayAvail
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'wlwhfkdm203833yedndnddkj939733heneee'
 
@@ -7,9 +9,13 @@ app.config['SECRET_KEY'] = 'wlwhfkdm203833yedndnddkj939733heneee'
 def home():
     return render_template('home.html')
 
-@app.route("/avail")
+@app.route("/avail", methods=["GET", "POST"])
 def avail():
-    return render_template('avail.html')
+    form = WeekDayAvail()
+    if form.validate_on_submit():
+        flash(f'Thank you, {form.name.data} for submitting your availability!', 'success')
+        return redirect(url_for('home'))
+    return render_template('avail.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,6 +1,23 @@
 from flask_wtf import FlaskForm
-from wtfforms import BooleanField
+from wtforms import BooleanField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 class WeekDayAvail(FlaskForm):
+    name = StringField('Enter name here...', validators=[DataRequired()])
+
     sunday = BooleanField('Sunday')
+
+    monday = BooleanField('Monday')
+
+    tuesday = BooleanField('Tuesday')
+
+    wednesday = BooleanField('Wednesday')
+
+    thursday = BooleanField('Thrusday')
+
+    friday = BooleanField('Friday')
+
+    saturday = BooleanField('Saturday')
+
+    submit = SubmitField('Submit')
     
